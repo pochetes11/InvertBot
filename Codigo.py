6c8216353cac858539c05ac7b492c572e0912fe7
matias <matiasarielmosquera@gmail.com>
2024-06-06 18:21:08 -0300

    añadi nuevas cosas como video, mensaje y audio, pero nada importante

--- Codigo.py
+++ Codigo.py
@@ -9,10 +9,24 @@ async def on_ready():
  
 @client.event
 async def on_message(message):
+    print("message-->", message)
+ 
     if message.author == client.user:
         return
  
-    if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
-
+    if message.content.startswith('hola'):
+        await message.channel.send('Hola!')
+ 
+    if message.content.startswith('imagen'):
+        await message.channel.send(file=discord.File('download.jpg'))
+ 
+    if message.content.startswith('video'):
+        await message.channel.send(file=discord.File('sample-mp4-file-small.mp4'))
+ 
+    if message.content.startswith('audio'):
+        await message.channel.send(file=discord.File('file_example_MP3_700KB.mp3'))
+ 
+    if message.content.startswith('file'):
+        await message.channel.send(file=discord.File('sample.pdf'))
+ 
 client.run('<Your Bot Token>')
